9884590e5eaa8bd2aa5629ad14fb880bc9b14e6a
= <=>
2021-04-08 11:26:36 -0400

    first commit

---
+++ WSOE/ViewController.swift
@@ -0,0 +1,37 @@
+
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController, AVAssetResourceLoaderDelegate {
+
+    
+    @IBOutlet weak var textLabel: UITextField!
+    
+    
+    
+    @IBOutlet weak var stateSwitch: UISwitch!
+    
+    
+    let urlFile = URL(string:"http://audio-ice2.ibiblio.org:8000/wsoe.mp3")!
+
+    private var avPlayer:AVPlayer!
+    private var avAudioSession:AVAudioSession = AVAudioSession.sharedInstance()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+        try! avAudioSession.setCategory(.playback, mode: .default, options: [])
+        try! avAudioSession.setActive(true)
+
+        let audioURLAsset = AVURLAsset(url: urlFile)
+        //audioURLAsset.resourceLoader.setDelegate(self, queue:DispatchQueue.init(label: "MY QUEUE"))
+        avPlayer = AVPlayer(playerItem: AVPlayerItem(asset: audioURLAsset))
+
+        
+        // add toggle button
+        avPlayer.play()
+
+    }
+
+}
