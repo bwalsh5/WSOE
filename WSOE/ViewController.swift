477f659b6e48de2a0e38e7f2b2313c68beaeb1eb
Matt Harrell <matthew.629@hotmail.com>
2021-04-14 20:16:27 -0400

    made url button, had to delete the switch because of error (staate typo).

--- WSOE/ViewController.swift
+++ WSOE/ViewController.swift
@@ -9,29 +9,37 @@ class ViewController: UIViewController, AVAssetResourceLoaderDelegate {
     
     
     
-    @IBOutlet weak var stateSwitch: UISwitch!
-    
-    
-    let urlFile = URL(string:"http://audio-ice2.ibiblio.org:8000/wsoe.mp3")!
+//    @IBOutlet weak var stateSwitch: UISwitch!
+//    
+//    
+//    let urlFile = URL(string:"http://audio-ice2.ibiblio.org:8000/wsoe.mp3")!
+//
+//    private var avPlayer:AVPlayer!
+//    private var avAudioSession:AVAudioSession = AVAudioSession.sharedInstance()
+//
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//
+//        
+//        try! avAudioSession.setCategory(.playback, mode: .default, options: [])
+//        try! avAudioSession.setActive(true)
+//
+//        let audioURLAsset = AVURLAsset(url: urlFile)
+//        //audioURLAsset.resourceLoader.setDelegate(self, queue:DispatchQueue.init(label: "MY QUEUE"))
+//        avPlayer = AVPlayer(playerItem: AVPlayerItem(asset: audioURLAsset))
+//
+//        
+//        // add toggle button
+//        avPlayer.play()
+//
+//    }
 
-    private var avPlayer:AVPlayer!
-    private var avAudioSession:AVAudioSession = AVAudioSession.sharedInstance()
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
+   
+    @IBAction func WSOE_btn(_ sender: UIButton) {
+        UIApplication.shared.open(URL(string: "https://wsoeelon.com")! as URL, options:[:], completionHandler: nil)
         
-        try! avAudioSession.setCategory(.playback, mode: .default, options: [])
-        try! avAudioSession.setActive(true)
-
-        let audioURLAsset = AVURLAsset(url: urlFile)
-        //audioURLAsset.resourceLoader.setDelegate(self, queue:DispatchQueue.init(label: "MY QUEUE"))
-        avPlayer = AVPlayer(playerItem: AVPlayerItem(asset: audioURLAsset))
-
-        
-        // add toggle button
-        avPlayer.play()
-
     }
+    
+   
 
 }
